9d85539fe5a137ee592e70e7186e5bb98cd9b6a6
chamer79 <chamer79@gmail.com>
2024-07-02 19:42:31 -0400

    completed problem 9

--- script.js
+++ script.js
@@ -210,10 +210,22 @@ Example: basicCalculator(10, 5, 'subtract') should return 5.
 
 Complete the exercise in the space below:
 */
-
+function basicCalculator(num1, num2, operation){
+    if(operation === "add") {
+        return(num1 + num2)
+    } else if(operation === "subtract"){
+        return(num1 - num2)
+    } else if(operation === "multiply"){
+        return(num1 * num2)
+    } else if(operation === "divide"){
+        return(num1 / num2)
+    } else {
+        return("Please enter a basic math operation: add, subtract, multiple, or divide.")
+    }
+}
 
 // -- UNCOMMENT LINE BELOW --
-// console.log('Exercise 9 Result:', basicCalculator(10, 5, "subtract"));
+console.log('Exercise 9 Result:', basicCalculator(10, 5, "subtract"));
 
 
 //   ***** LEVEL UP *****
